cc295ed82330962088187bfc9bd3b43e7d77c188
karnjj <karnjj@hotmail.com>
2022-08-24 19:01:13 +0700

    feat: link root path to user profile

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  get 'user/profile'
+  get root to: 'user#profile'
+  
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
